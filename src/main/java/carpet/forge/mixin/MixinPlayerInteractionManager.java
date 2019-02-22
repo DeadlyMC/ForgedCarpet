bfaf7ee2d29b3f1c2f5e17dd2d4b854d66460ea2
arnevdl29 <arnevdl@gmail.com>
2019-02-22 13:28:29 +0100

    auto formatted code

--- src/main/java/carpet/forge/mixin/MixinPlayerInteractionManager.java
+++ src/main/java/carpet/forge/mixin/MixinPlayerInteractionManager.java
@@ -24,43 +24,38 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(value = PlayerInteractionManager.class)
-public abstract class MixinPlayerInteractionManager
-{
+public abstract class MixinPlayerInteractionManager {
 
-    @Shadow
-    public World world;
+	@Shadow
+	public World world;
 
-    @Shadow
-    public EntityPlayerMP player;
+	@Shadow
+	public EntityPlayerMP player;
 
-    @Shadow
-    private BlockPos destroyPos = BlockPos.ORIGIN;
-    
-    @Shadow public abstract boolean isCreative();
-    
-    @Shadow private GameType gameType;
+	@Shadow
+	private BlockPos destroyPos = BlockPos.ORIGIN;
 
-    @Inject(method = "onBlockClicked", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/world/World;sendBlockBreakProgress(ILnet/minecraft/util/math/BlockPos;I)V", shift = At.Shift.BEFORE))
-    private void notifyUpdate(BlockPos pos, EnumFacing side, CallbackInfo ci)
-    {
-        if (CarpetSettings.getBool("miningGhostBlocksFix"))
-        {
-            player.connection.sendPacket(new SPacketBlockChange(world, destroyPos));
-        }
-    }
+	@Shadow
+	public abstract boolean isCreative();
 
-    @Inject(method="processRightClickBlock", cancellable = true, at=@At(value= "INVOKE",
-    		target="Lnet/minecraft/block/Block;onBlockActivated(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/util/EnumHand;Lnet/minecraft/util/EnumFacing;FFF)Z", shift= At.Shift.BEFORE), locals= LocalCapture.CAPTURE_FAILHARD)
-    private void checkFlipCactus(EntityPlayer player, World worldIn, ItemStack stack,
-            EnumHand hand, BlockPos pos, EnumFacing facing, float hitX, float hitY,
-            float hitZ, CallbackInfoReturnable<EnumActionResult> cir,
-            PlayerInteractEvent.RightClickBlock event, double reachDist, EnumActionResult result, boolean bypass,
-            IBlockState iblockstate)
-    {
-    	boolean flipped = BlockRotator.flipBlockWithCactus(worldIn, pos, iblockstate, player, hand, facing, hitX, hitY, hitZ);
-		if (flipped)
-		{
+	@Shadow
+	private GameType gameType;
+
+	@Inject(method = "onBlockClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;sendBlockBreakProgress(ILnet/minecraft/util/math/BlockPos;I)V", shift = At.Shift.BEFORE))
+	private void notifyUpdate(BlockPos pos, EnumFacing side, CallbackInfo ci) {
+		if (CarpetSettings.getBool("miningGhostBlocksFix")) {
+			player.connection.sendPacket(new SPacketBlockChange(world, destroyPos));
+		}
+	}
+
+	@Inject(method = "processRightClickBlock", cancellable = true, at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;onBlockActivated(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/util/EnumHand;Lnet/minecraft/util/EnumFacing;FFF)Z", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
+	private void checkFlipCactus(EntityPlayer player, World worldIn, ItemStack stack, EnumHand hand, BlockPos pos,
+			EnumFacing facing, float hitX, float hitY, float hitZ, CallbackInfoReturnable<EnumActionResult> cir,
+			PlayerInteractEvent.RightClickBlock event, double reachDist, EnumActionResult result, boolean bypass,
+			IBlockState iblockstate) {
+		boolean flipped = BlockRotator.flipBlockWithCactus(worldIn, pos, iblockstate, player, hand, facing, hitX, hitY,
+				hitZ);
+		if (flipped) {
 			cir.setReturnValue(EnumActionResult.PASS);
 		}
 	}
